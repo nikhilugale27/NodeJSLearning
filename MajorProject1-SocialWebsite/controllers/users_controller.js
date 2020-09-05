bcd93b26d84283a491c6e6484c1538c025953838
nikhilugale27 <nikhilugale76@gmail.com>
2020-09-05 22:48:27 +0530

    Implementation of the Partials

--- MajorProject1-SocialWebsite/controllers/users_controller.js
+++ MajorProject1-SocialWebsite/controllers/users_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>This is the User Profile Page.</h1>')
+    return res.render('user_profile', {
+        title: "User Profile"
+    });
 }
