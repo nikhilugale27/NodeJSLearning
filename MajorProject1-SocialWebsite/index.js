eaeb6d9c00fc01d65a5f703b3db3076583d0b99a
nikhilugale27 <nikhilugale76@gmail.com>
2020-09-06 21:53:15 +0530

    Linking the MongoDB using the MOngoose

--- MajorProject1-SocialWebsite/index.js
+++ MajorProject1-SocialWebsite/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 9000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 //setting up the static assets
 app.use(express.static('./assets'));
