556f552fd19e6a2def2c3527d6ad7841cf8a4a43
nikhilugale27 <nikhilugale76@gmail.com>
2020-09-12 00:15:13 +0530

    passport local strategy file corrected

--- MajorProject1-SocialWebsite/config/passport-local-strategy.js
+++ MajorProject1-SocialWebsite/config/passport-local-strategy.js
@@ -1,15 +1,15 @@
 const passport = require('passport');
 
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
 
-//authentication using the passport
+//authentication using passport
 passport.use(new LocalStrategy({
         usernameField:'email'
     },
     function(email, password, done){
-        //find the user
+        //find the user and establish the identity
         User.findOne({email:email},function(err,user){
             if(err){
                 console.log('Error in getting the passport data');
