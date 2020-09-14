bcbbb4da8624657fff09ce862d6f2057ffdedb84
nikhilugale27 <nikhilugale76@gmail.com>
2020-09-15 00:09:53 +0530

    Adding the posts to DB

--- MajorProject1-SocialWebsite/routes/index.js
+++ MajorProject1-SocialWebsite/routes/index.js
@@ -7,6 +7,7 @@ console.log('Home Router Loaded....');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 // for anty further routes, access from here
 // router.use('/routerName', require('./routerfile));
