61f35087c4ac19d377190ec9c4fa60b41364f892
nikhilugale27 <nikhilugale76@gmail.com>
2020-09-21 14:52:11 +0530

    Added more messages using the noty

--- MajorProject1-SocialWebsite/controllers/posts_controller.js
+++ MajorProject1-SocialWebsite/controllers/posts_controller.js
@@ -7,10 +7,12 @@ module.exports.create = async function(req,res){
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
     }
     catch(err)
     {
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
 }
@@ -24,14 +26,16 @@ module.exports.destroy = async function(req, res)
             post.remove();
     
             await Comment.deleteMany({post: req.params.id});
+            req.flash('success', 'Post and associated comments deleted!');
             return res.redirect('back');
         }else{
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
     }
     catch(err)
     {
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
 }
