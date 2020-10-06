bf478637b3ef7176120898c62dd8e840be4f8db1
nikhilugale27 <nikhilugale76@gmail.com>
2020-10-06 23:08:21 +0530

    AJAX for the comment creation and deletion added

--- MajorProject1-SocialWebsite/controllers/comments_controller.js
+++ MajorProject1-SocialWebsite/controllers/comments_controller.js
@@ -1,52 +1,80 @@
+const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-const Comment = require('../models/comment');
+module.exports.create = async function(req, res){
 
-module.exports.create = async function(req,res){
-    try
-    {
+    try{
         let post = await Post.findById(req.body.post);
-        if(post)
-        {
-                let comment = await Comment.create({
-                    content: req.body.content,
-                    post: req.body.post,
-                    user: req.user._id
+
+        if (post){
+            let comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            });
+
+            post.comments.push(comment);
+            post.save();
+
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
                 });
-                    
-                post.comments.push(comment);
-                post.save();
-                req.flash('success', 'Comment published!');
-                res.redirect('/');
+            }
+
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
         }
-    }catch(err)
-    {
+    }catch(err){
         req.flash('error', err);
         return;
     }
+    
 }
 
-module.exports.destroy = async function(req, res)
-{
+
+module.exports.destroy = async function(req, res){
+
     try{
         let comment = await Comment.findById(req.params.id);
-        if(comment.user == req.user.id)
-        {
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
+
             comment.remove();
-            
-            let post = Post.findByIdAndUpdate(postId, {$pull:{comments: req.params.id}});
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
             req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
-        }
-        else
-        {
+        }else{
             req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    }catch(err)
-    {
+    }catch(err){
         req.flash('error', err);
         return;
     }
+    
 }
