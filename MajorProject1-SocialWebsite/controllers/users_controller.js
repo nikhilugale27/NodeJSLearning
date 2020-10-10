420311f66427195b879a621596a513d5d457acbd
nikhilugale27 <nikhilugale76@gmail.com>
2020-10-10 15:31:32 +0530

    Saving the file from the Params

--- MajorProject1-SocialWebsite/controllers/users_controller.js
+++ MajorProject1-SocialWebsite/controllers/users_controller.js
@@ -10,13 +10,46 @@ module.exports.profile = function(req,res){
 }
 
 
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id){
+module.exports.update = async function(req, res){
+    /*if(req.user.id == req.params.id){
         User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
             req.flash('success', 'Updated!');
             return res.redirect('back');
         });
     }else{
+        req.flash('error', 'Unauthorized!');
+        return res.status(401).send('Unauthorized');
+    }*/
+
+    //to upload the file
+    if(req.user.id == req.params.id)
+    {
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err){
+                    console.log('******* Multer Error*******');
+                }
+                //console.log(req.file);
+                user.name = req.body.name;
+                user.email = req.body.emai;
+                if(req.file){
+                    //saving the path of uploaded file in user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }
+        catch(err)
+        {
+            req.flash('error', err); 
+            return res.redirect('back');
+        }
+    }
+    else
+    {
         req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unauthorized');
     }
--- MajorProject1-SocialWebsite/models/user.js
+++ MajorProject1-SocialWebsite/models/user.js
@@ -25,6 +25,22 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now()); 
+    }
+  });
+
+
+// static functions
+
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
