68a25141fad13216bcb634cda461ea4598ed1d9f
nikhilugale27 <nikhilugale76@gmail.com>
2020-10-30 16:59:55 +0530

    File added for Likes and Counts

--- MajorProject1-SocialWebsite/controllers/home_controller.js
+++ MajorProject1-SocialWebsite/controllers/home_controller.js
@@ -2,28 +2,11 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 module.exports.home = async function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
-    // populate the user of each post
-    //Post.find({}).populate('user').exec(function(err, posts){
-    //    return res.render('home', {
-    //        title: "Codeial | Home",
-    //        posts:  posts
-    //    });
-    // })
-
 
     // populate the user of each post
     try
     {
+        // Chnage :: populate the likes of each post and comment
         let posts = await Post.find({})
         .sort('-createdAt')
         .populate('user')
@@ -31,8 +14,11 @@ module.exports.home = async function(req, res){
             path: 'comments',
             populate: {
                 path: 'user'
+            },
+            populate:{
+                path: 'likes'
             }
-        })
+        }).populate('comments').populate('likes');
 
         let users = await User.find({});
                     
@@ -47,5 +33,3 @@ module.exports.home = async function(req, res){
         return;
     }
 }
-
-// module.exports.actionName = function(req, res){}
